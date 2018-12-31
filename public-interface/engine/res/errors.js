7e4f657ec5d0e5533649f182074795429986d165
Marcel Wagner <wagmarcel@web.de>
2018-12-31 15:24:25 +0100

    Error code for partial not found is now unique

--- public-interface/engine/res/errors.js
+++ public-interface/engine/res/errors.js
@@ -119,8 +119,8 @@ module.exports = {
             InvalidParameterValues: {code: 5412, status: 400, message: "Invalid parameter values"}
         },
         Data: {
-            PartialDataProcessed: {code: 7400, status: 400, message: "Only part of the data has been submitted"},
             InvalidData: {code: 6400, status: 400, message: "Invalid data for Target Filter"},
+            PartialDataProcessed: {code: 6402, status: 404, message: "Only part of the data has been submitted successfully"},
             FormatError: {code: 6500, status: 500, message: "Format not accepted"},
             OffsetAndLimitBothOrNoneRequired:{code:6504, status:404, message:"offset and limit must be specified both or none"},
             WrongResponseCodeFromAA: {code: 6506, status: 500, message: "Could not send data."},
